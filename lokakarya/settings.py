e0585ee6c0550a543fde9bd73555a181f2ec82a9
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-10-09 20:10:08 +0700

    Trying to solve static folder on PWS

--- lokakarya/settings.py
+++ lokakarya/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
