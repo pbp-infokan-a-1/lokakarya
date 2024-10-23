c7be86fb07f664c51a77a3a19466ad754f331d97
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-10-23 13:36:02 +0700

    Update settings.py

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import profile, update_profile
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'userprofile'
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('profile/update/', update_profile, name='update_profile'),  # This is the missing pattern
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
