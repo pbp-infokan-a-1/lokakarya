c0054e56bdd7546577c0eac0e518df56179788af
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-10-24 23:51:36 +0700

    Fixing navbar for desktop

--- lokakarya/settings.py
+++ lokakarya/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-!n5f81yk&+d50p$z1jq(6x51k@ek@#y%8#l!2+cg0qwg2*uv!z
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = False
+DEBUG = not PRODUCTION
+
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "belva-ghani-lokakarya.pbp.cs.ui.ac.id"]
 
 
