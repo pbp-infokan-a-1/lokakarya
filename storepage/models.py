705c593c3cbc4410e534f5a37e76062acd47b8ca
yasweh <myayyas@gmail.com>
2024-10-24 09:43:01 +0700

    memasukkan dataset store ke database dan membuat kerangka store page

--- storepage/models.py
+++ storepage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Toko(models.Model):
+    nama = models.CharField(max_length=200)
+    hari_buka = models.CharField(max_length=100)
+    alamat = models.TextField()
+    email = models.EmailField()
+    telepon = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nama
+
+    class Meta:
+        verbose_name_plural = "Toko"
--- storepage/views.py
+++ storepage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Toko
 
-# Create your views here.
+def toko_list(request):
+    toko_list = Toko.objects.all()
+    return render(request, 'storepage.html', {'toko_list': toko_list})
