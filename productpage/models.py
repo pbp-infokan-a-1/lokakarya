a7aa1520322ac3fa7451f2e10f7265ee38c6b88c
ThatTryHard <orlando.devito@ui.ac.id>
2024-10-25 16:14:13 +0700

    lala

--- productpage/models.py
+++ productpage/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from storepage.models import Toko
+from storepage.models import Toko
 # from storepage.models import Store
 
 class Category(models.Model):
