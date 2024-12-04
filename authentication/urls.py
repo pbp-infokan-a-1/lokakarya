c1c5bcb6fe88e430b709fc9075b1f51afd0a78dd
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-12-04 14:03:21 +0700

    push to pws

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from authentication.views import register, login_user, logout_user, login
+from authentication.views import register, login_user, logout_user, login, register_app
 
 app_name = 'authentication'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('login_app/', login, name='login_app'),
+    path('register_app/', register_app, name='register_app'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+from django.contrib.auth.models import User
+import json
 import datetime
 
 def register(request):
@@ -69,3 +71,41 @@ def login(request):
             "status": False,
             "message": "Login gagal, periksa kembali email atau kata sandi."
         }, status=401)
+  
+@csrf_exempt
+def register_app(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data['username']
+        password1 = data['password1']
+        password2 = data['password2']
+
+        # Check if the passwords match
+        if password1 != password2:
+            return JsonResponse({
+                "status": False,
+                "message": "Passwords do not match."
+            }, status=400)
+        
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                "status": False,
+                "message": "Username already exists."
+            }, status=400)
+        
+        # Create the new user
+        user = User.objects.create_user(username=username, password=password1)
+        user.save()
+        
+        return JsonResponse({
+            "username": user.username,
+            "status": 'success',
+            "message": "User created successfully!"
+        }, status=200)
+    
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Invalid request method."
+        }, status=400)
