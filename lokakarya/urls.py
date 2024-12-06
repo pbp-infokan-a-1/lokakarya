1c5d73c55c728514f8852e38a1003a9330d1831b
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2024-12-06 23:29:19 +0700

    fetch data profile to pws 4

--- lokakarya/urls.py
+++ lokakarya/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('userprofile/', include('landingpage.urls')),
+    path('', include('landingpage.urls')),
     path('', include('storepage.urls')),
-    path('', include('userprofile.urls')),
+    path('userprofile/', include('userprofile.urls')),
     path('', include('productpage.urls')),
     path('', include('forumandreviewpage.urls')),
     path('', include('adminDashboard.urls')),
